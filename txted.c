1f4014d8b1c409691e7b4ba6297e21064c4ffa5b
pjr.monto@gmail.com <pjr.monto@gmail.com>
2025-07-04 18:56:05 +0200

    the main is cleaner now with specific functions that handle terminal screen refreshing, and key processing.

--- txted.c
+++ txted.c
@@ -5,6 +5,13 @@
 #include <unistd.h>
 #include <termios.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+/**  Map character to Ctrl+<key> combo (e.g., 'A' -> Ctrl+A)
+ * C doens't have binary literals so we use hexa since it's also more concise, it maps to 00011111, which means that we are disabling the
+ * ctrl-key equivalent of that key using the bitwise AND stripping bit 5 and 6.
+ */
+
+/** REGION: data */
 struct termios og_termios;
 
 void die(const char *s)
@@ -12,7 +19,7 @@ void die(const char *s)
     perror(s);
     exit(1);
 }
-
+/** REGION: terminal */
 void disableRawMode(void)
 {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios) == -1)
@@ -63,30 +70,58 @@ void enableRawMode(void)
         die("tcsetattr"); // set
 }
 
+char editorReadKey()
+{
+    // read 1 byte from the std input into char c. Keep doing it until there are no more bytes to read. Returns the n of bytes it read.
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+
+    return c;
+}
+
+/** REGION: input */
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
+/** REGION: output */
+void editorRefreshScren()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    // Write 4 bytes to the terminal: an escape sequence (\x1b[2J) that clears the screen.
+    // Escape sequences start with \x1b (ESC) and control terminal formatting.
+    // The escape sequence \x1b[2J uses the 'J' command (Erase In Display) with argument 2 to clear the entire screen.
+    // Other options: [0J clears from cursor to end, [1J clears from start to cursor, [J defaults to [0J.
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    // Repositions the cursor at the top left side of the screen
+    // The escape sequence \x1b[H uses the 'H' command (Cursor Position) to move the cursor.
+    // It takes two arguments: row and column (e.g., \x1b[12;40H centers the cursor on an 80×24 screen).
+}
+
+/** REGION: init */
 int main(void)
 {
     enableRawMode();
 
-    // Switched the terminal from canonical to raw mode
-    // read 1 byte from the std input into char c. Keep doing it until there are no more bytes to read. Returns the n of bytes it read.
     while (1)
     {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-
-        if (iscntrl(c)) // test wether a char is control char. (non printable characters)
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-
-        if (c == 'q')
-            break;
+        editorRefreshScren();
+        editorProcessKeypress();
     }
 
-    return 0; // return 0 at the end of the file
+    return 0;
 }
